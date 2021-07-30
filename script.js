7a50603f085b02e34fb136a3d0cbb055c95b0ca2
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-07-30 21:40:21 +0300

    lesson02 is ready

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-alert('Hello, World!');
-
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-
-console.log('learn js');
+let money = 7000;
+let income = '3000';
+let addExpenses = 'Коммуналка, Аренда, Топливо, Салон, Шоппинг';
+let deposit = true;
+let mission = 1000000000000000;
+let period = 8;
+let budgetDay = (7000/30);
 
 
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев. Цель заработать ${mission} долларов`);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
