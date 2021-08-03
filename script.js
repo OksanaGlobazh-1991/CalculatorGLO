16238816c5b15231644bcbda5989f11bfd9b6be5
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-08-03 10:21:37 +0300

    lesson03!!!

--- script.js
+++ script.js
@@ -36,9 +36,9 @@ console.log(`Бюджет на день ${Math.floor(budgetDay)}`);
 
 if (budgetDay >= 1200) {
   console.log('У вас высокий уровень дохода');
-} else if (budgetDay < 1200 && budgetDay >= 600) {
+} else if (budgetDay >= 600) {
   console.log('У вас средний уровень дохода');
-} else if (budgetDay < 600 && budgetDay >= 0) {
+} else if (budgetDay >= 0) {
   console.log('К сожалению у вас уровень дохода ниже среднего');
 } else if (budgetDay < 0) {
   console.log('Что то пошло не так');
