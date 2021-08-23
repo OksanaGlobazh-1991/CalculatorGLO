237ce2e5395fc9dda49cb1ab7182b50d3a80f3b0
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-08-23 18:39:41 +0300

    lesson15/2

--- FC/scriptFC.js
+++ FC/scriptFC.js
@@ -1,26 +1,43 @@
-function DomElement(selector, height, width, bg, fontSize){
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-  this.getElement = function(){
-    if(this.selector[0] === '.') {
-      let div1 = document.createElement('div');
-      div1.className = 'selector';
-      div1.style.cssText = `height: ${height}px; width: ${width}px; background: ${bg}; font-size: ${fontSize}px`;
-      div1.textContent = 'есть надежда';
-      document.body.append(div1);
+// function DomElement(selector, height, width, bg, fontSize){
+//   this.selector = selector;
+//   this.height = height;
+//   this.width = width;
+//   this.bg = bg;
+//   this.fontSize = fontSize;
+//   this.getElement = function(){
+//     if(this.selector[0] === '.') {
+//       let div1 = document.createElement('div');
+//       div1.className = 'selector';
+//       div1.style.cssText = `height: ${height}px; width: ${width}px; background: ${bg}; font-size: ${fontSize}px`;
+//       div1.textContent = 'есть надежда';
+//       document.body.append(div1);
   
-    } else if(this.selector[0] === '#') {
-      let div1 = document.createElement('p');
-      div1.setAttribute = ('id', 'selector');
-      document.body.append(div1);
-      div1.style.cssText = `height: ${height}px; width: ${width}px; background: ${bg}; font-size: ${fontSize}px`;
-      div1.textContent = 'есть надежда'; 
-      };
-  };
-}
+//     } else if(this.selector[0] === '#') {
+//       let div1 = document.createElement('p');
+//       div1.setAttribute = ('id', 'selector');
+//       document.body.append(div1);
+//       div1.style.cssText = `height: ${height}px; width: ${width}px; background: ${bg}; font-size: ${fontSize}px`;
+//       div1.textContent = 'есть надежда'; 
+//       };
+//   };
+// }
 
-const home = new DomElement ('.doors', '100', '100', 'red', '15');
-home.getElement();
+// const home = new DomElement ('.doors', '100', '100', 'red', '15');
+// home.getElement();
+
+class First {
+  hello(){
+    console.log('Привет я метод родителя!');
+  }
+};
+
+class Second extends First {
+  hello(){
+    super.hello();
+    console.log('А я наследуемый метод!');
+  }
+  
+};
+
+let second = new Second();
+console.log(second.hello());
