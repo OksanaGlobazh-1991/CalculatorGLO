fc377b9e9daf30c994da55bf1f34de9ae702ca1e
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-08-24 22:23:37 +0300

    lesson16/2

--- FC/scriptFC.js
+++ FC/scriptFC.js
@@ -25,19 +25,49 @@
 // const home = new DomElement ('.doors', '100', '100', 'red', '15');
 // home.getElement();
 
-class First {
-  hello(){
-    console.log('Привет я метод родителя!');
-  }
-};
+// class First {
+//   hello(){
+//     console.log('Привет я метод родителя!');
+//   }
+// };
 
-class Second extends First {
-  hello(){
-    super.hello();
-    console.log('А я наследуемый метод!');
-  }
+// class Second extends First {
+//   hello(){
+//     super.hello();
+//     console.log('А я наследуемый метод!');
+//   }
   
-};
+// };
 
-let second = new Second();
-console.log(second.hello());
+// let second = new Second();
+// console.log(second.hello());
+
+const myLesson = [
+  {lesson: 1, type: 'basic', points: 2},
+  {lesson: 2, type: 'additional', points: 4},
+  {lesson: 3, type: 'basic', points: 6},
+  {lesson: 4, type: 'additional', points: 3},
+  {lesson: 5, type: 'basic', points: 4},
+  {lesson: 6, type: 'basic', points: 2},
+  {lesson: 7, type: 'additional', points: 2},
+  {lesson: 8, type: 'basic', points: 6},
+  {lesson: 9, type: 'basic', points: 4},
+  {lesson: 10, type: 'basic', points: 6},
+  {lesson: 11, type: 'additional', points: 5}, 
+  {lesson: 12, type: 'basic', points: 2}, 
+  {lesson: 13, type: 'additional', points: 2}, 
+  {lesson: 14, type: 'basic', points: 4},
+  {lesson: 15, type: 'additional', points: 1},
+  {lesson: 16, type: 'additional', points: 7},
+];
+
+for (let i = 0; i < myLesson.length; i++) {
+  if (myLesson[i].type === 'basic') {
+    myLesson[i].points /= 2;
+  } else {
+    myLesson.splice(i, 1);
+    i--;
+  }
+}
+
+console.log(myLesson);
